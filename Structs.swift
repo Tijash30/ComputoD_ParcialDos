0981a901afe36dbea7f35fef633491db4e3e8413
Isakinho <100299127+Isakinho@users.noreply.github.com>
2024-04-16 15:08:50 -0600

    Add files via upload

---
+++ Structs.swift
@@ -0,0 +1,78 @@
+//
+//  Structs.swift
+//  conecta4
+//
+//  Created by Marcelo Preciado Fausto on 13/04/24.
+//
+
+import Foundation
+import SwiftUI
+
+
+struct PlayerMessage: Decodable, Encodable{
+    var inst : String = ""
+    var id : Int = 0
+    var fila : Int = 0
+    var columna : Int = 0
+}
+
+struct PlayerInfo: Decodable, Encodable{
+    var inst : String = ""
+    var user : String = ""
+}
+
+struct IDinfo: Decodable, Encodable{
+    var inst : String = ""
+    var id : Int = 0
+}
+
+class Player: ObservableObject{
+    static var shared : Player = Player(user: "", email: "", password: "")
+    @Published var update : Bool = false
+    @Published var errorMessage : String = ""
+    @Published var loginCorrect : Bool = false
+    @Published var ID : Int = 0
+    @Published var win : String = ""
+    
+    init(user: String, email: String, password: String) {
+        self.user = user
+        self.email = email
+        self.password = password
+    }
+    var user : String = ""
+    var email : String = ""
+    var password : String = ""
+}
+
+struct CircleStruct{
+    var color: Color
+    let position: Int
+}
+
+
+
+class Tablero: ObservableObject{
+    static var shared : Tablero = Tablero()
+    @Published var update : Bool = false
+    @Published var tablero: [[CircleStruct]] = {
+        var initialTablero: [[CircleStruct]] = []
+        for _ in 0..<7 {
+            var row: [CircleStruct] = []
+            for column in 0..<7 {
+                row.append(CircleStruct(color: .gray, position: column))
+            }
+            initialTablero.append(row)
+        }
+        return initialTablero
+    }()
+
+    func agregarFicha(ficha: PlayerMessage){
+        print("fila: \(ficha.fila), columna: \(ficha.columna), id: \(ficha.id)")
+        
+        if ficha.id == 1{
+            tablero[ficha.fila][ficha.columna].color = .red
+        }else if ficha.id == 2{
+            tablero[ficha.fila][ficha.columna].color = .yellow
+        }
+    }
+}
